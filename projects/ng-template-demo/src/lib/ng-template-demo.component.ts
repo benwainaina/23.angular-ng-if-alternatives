4dc5f98ec96af7de5a7aec8b9f396fc13712e681
ben wainaina <bwainainamwangi@gmail.com>
2024-10-29 14:12:23 +0300

    feat: adding ng-template demo

--- projects/ng-template-demo/src/lib/ng-template-demo.component.ts
+++ projects/ng-template-demo/src/lib/ng-template-demo.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {
+  AsyncPipe,
+  NgFor,
+  NgSwitch,
+  NgSwitchCase,
+  NgTemplateOutlet,
+} from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { SharedService } from '../../../shared/src/public-api';
 
 @Component({
   selector: 'lib-ng-template-demo',
   standalone: true,
-  imports: [],
+  imports: [NgSwitch, NgSwitchCase, NgTemplateOutlet, AsyncPipe, NgFor],
   templateUrl: 'ng-template-demo.component.html',
   styleUrl: 'ng-template-demo.component.scss',
 })
-export class NgTemplateDemoComponent {}
+export class NgTemplateDemoComponent {
+  public sharedService: SharedService = inject(SharedService);
+  public userTypeResponsibilities = {
+    admin: [
+      'Reset all accounts credentials',
+      'Schedule all departments meetings',
+      'Make company-wide announcement',
+    ],
+    supervisor: [
+      'Manage employee accounts in your departments',
+      'Schedule departmental meetings',
+      'Make departmental announcements',
+    ],
+    employee: [
+      'Log when you have entered the office',
+      'Make requests for equipments like paper clips etc',
+      'Request credentials re-setting',
+    ],
+  };
+}
