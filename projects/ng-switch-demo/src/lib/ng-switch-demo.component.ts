600129b87a6e84930d9e41cf7d05482cf47cfe15
ben wainaina <bwainainamwangi@gmail.com>
2024-10-29 13:01:35 +0300

    feat: add ng-switch demo

--- projects/ng-switch-demo/src/lib/ng-switch-demo.component.ts
+++ projects/ng-switch-demo/src/lib/ng-switch-demo.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SharedService } from '../../../shared/src/public-api';
+import {
+  AsyncPipe,
+  NgFor,
+  NgIf,
+  NgSwitch,
+  NgSwitchCase,
+  NgTemplateOutlet,
+} from '@angular/common';
 
 @Component({
   selector: 'lib-ng-switch-demo',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgSwitch, NgSwitchCase, NgTemplateOutlet, AsyncPipe],
   templateUrl: 'ng-switch-demo.component.html',
-  styles: ``,
+  styleUrl: 'ng-switch-demo.component.scss',
 })
-export class NgSwitchDemoComponent {}
+export class NgSwitchDemoComponent {
+  public sharedService: SharedService = inject(SharedService);
+  public userTypeResponsibilities = {
+    admin: [
+      'Reset all accounts credentials',
+      'Schedule all departments meetings',
+      'Make company-wide announcement',
+    ],
+    supervisor: [
+      'Manage employee accounts in your departments',
+      'Schedule departmental meetings',
+      'Make departmental announcements',
+    ],
+    employee: [
+      'Log when you have entered the office',
+      'Make requests for equipments like paper clips etc',
+      'Request credentials re-setting',
+    ],
+  };
+}
